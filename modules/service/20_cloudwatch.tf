4f15aa328e26077ff7abb72fe2a506c464b4203c
bill-skdnd <mskim9262@sk.com>
2026-05-07 13:57:29 +0900

    init

--- modules/service/20_cloudwatch.tf
+++
@@ -1,7 +0,0 @@
-resource aws_cloudwatch_log_group "service" {
-  name              = "/ecs/${var.env}/${var.name}"
-  retention_in_days = 7
-  tags = {
-    Name        = var.name
-  }
-}
--- modules/service/output.tf
+++
@@ -1,7 +0,0 @@
-output "security_group" {
-  value = aws_security_group.service
-}
-
-output "task_role" {
-  value = aws_iam_role.task_role
-}
